3d00bd39915ae5a1815688633f39f860b9c23501
RezaAffandi34 <reza02affandi@gmail.com>
2021-04-13 15:01:51 +0700

    ResponsiPemrograman3370

---
+++ ResponsiPemrograman3370/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ResponsiPemrograman3370
+{
+    class Karyawan
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("No   NIK/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1. 190302123 Paijo   3,000,000");
+            Console.WriteLine("2. 190302124 Jono    2,000,000");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%!!");
+            Console.WriteLine();
+            Console.WriteLine("No   NIK/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1. 190302123 Paijo   3,300,000");
+            Console.WriteLine("2. 190302124 Jono    2,200,000");
+
+            Console.ReadKey();
+        }
+    }
+}
